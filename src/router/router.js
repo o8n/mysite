5d064116908be4220194c549ccc31fb720586ef3
okamotchan <m.okamotchan@gmail.com>
2019-12-15 15:17:40 +0900

    add about skill

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import about from '@/components/about';
-import skill from '@/components/skill';
-import home from '@/components/home';
+import Vue from 'vue'
+import Router from 'vue-router'
+import about from '@/components/about'
+import skill from '@/components/skill'
+import home from '@/components/home'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -12,12 +12,12 @@ export default new Router({
   routes: [{
     path: '/',
     component: home
-    },{
-      path: '/about',
-      component: about
-    }, {
-      path: '/skill',
-      component: skill
-    }
+  }, {
+    path: '/about',
+    component: about
+  }, {
+    path: '/skill',
+    component: skill
+  }
   ]
-});
+})
